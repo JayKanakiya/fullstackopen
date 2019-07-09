d1be59e8d8f77f06062886c1254db56211dbfb18
Jay Kanakiya <jaykanakiya42@gmail.com>
2019-07-09 12:47:40 +0530

    1.6 solution added

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    
+    return (
+        <div>
+            <h1> give feedback </h1>
+            <div>
+                <button onClick={()=> setGood(good+1)}>good</button>
+                <button onClick={()=> setNeutral(neutral+1)}>neutral</button>
+                <button onClick={()=> setBad(bad+1)}>bad</button>                
+            </div>
+            <h2>statistics</h2>
+            <p>good {good}</p>
+            <p>neutral {neutral}</p>
+            <p>bad {bad}</p>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
