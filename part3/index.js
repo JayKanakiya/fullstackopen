a49386512b2255cd4cb43a504c7ae1e284f6d0dd
Jay Kanakiya <jay.kanakiya@somaiya.edu>
2019-12-09 18:10:27 +0530

    3.1 solution

---
+++ part3/index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+
+let phonebook = [
+    {
+      id: 1,
+      name: "Arto Hellas",
+      number: "040-123456"
+    },
+    {
+      id: 2,
+      name: "Ada Lovelace",
+      number: "39-44-5323523"
+    },
+    {
+      id: 3,
+      name: "Dan Abramov",
+      number: "12-43-234345"
+    },
+    {
+      id: 4,
+      name: "Mary Poppendieck",
+      number: "39-23-6423122"
+    }
+  ]
+
+
+app.get('/',(req,res)=>{
+    res.send('<h1>Phonebook</h1>')
+})
+
+
+app.get('/api/persons',(req,res) => {
+    res.json(phonebook)
+    console.log(phonebook)
+})
+
+const port = 3001
+app.listen(port, ()=>{
+    console.log(`Server running on port ${port}`)
+})
