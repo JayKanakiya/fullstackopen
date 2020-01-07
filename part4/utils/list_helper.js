c430610171299eb1415c0813ce2abd575352cd5c
jaykanakiya <jay.kanakiya@somaiya.edu>
2020-01-07 21:16:26 +0530

    4.5 solution

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -10,7 +10,23 @@ const totalLikes = (blogs) => {
     return total
 }
 
+const favoriteBlog = (blogs) => {
+    max_likes = -1
+    for(var i=0;i<blogs.length;i++){
+        if(blogs[i]['likes'] > max_likes){
+            max_likes = blogs[i]['likes']
+        }
+    }
+
+    for(var i=0;i<blogs.length;i++){
+        if(blogs[i]['likes'] === max_likes){
+            return blogs[i]
+        }
+    }
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
